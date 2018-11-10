5dadb4fb38c6c16f89a327395ac5907d50450dcc
Fabian Forsström <fabian.forsstrom@gmail.com>
2018-11-10 01:23:26 +0100

    Made possible to update notes which also moves them to the top

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const notesOutput = document.querySelector('#notes-output');
 const notesArray = [];
+let domArray = [];
 let currentNote = '';
 
 // Initialize Quill editor
@@ -11,14 +12,26 @@ var quill = new Quill('#editor', {
 // Time can be added in future
 function getDate() {
   let d = new Date();
-  let date = [d.getFullYear(), d.getMonth(), d.getDate()];
-  if (date[2] <= 9) {
-    date[2] = '0' + date[2];
+  let date = [
+    [d.getFullYear(), d.getMonth(), d.getDate()],
+    [d.getHours(), d.getMinutes()]
+  ];
+
+  if (date[0][2] <= 9) {
+    date[0][2] = '0' + date[0][2];
   }
-  if (date[1] <= 1) {
-    date[1] = '0' + date[1];
+  if (date[0][1] <= 9) {
+    date[0][1] = '0' + date[0][1];
   }
-  return date.join('-');
+
+  if (date[1][0] <= 9) {
+    date[1][0] = '0' + date[1][0];
+  }
+  if (date[1][1] <= 9) {
+    date[1][1] = '0' + date[1][1];
+  }
+
+  return date[0].join('-') + ' ' + '@' + ' ' + date[1].join(':');
 }
 
 function noteConstructor(content) {
@@ -29,10 +42,12 @@ function noteConstructor(content) {
     deleted: false,
     title: noteTitle(),
     tags: [],
-    content: content
+    content: content,
   };
   return note;
 }
+
+// Old functions that got replaced by saveNote()
 
 // function storeNote() {
 //   if (currentNote.length > 5) {
@@ -51,22 +66,19 @@ function noteConstructor(content) {
 function newNote() {
   currentNote = '';
   quill.setContents('');
+  showNotes(buildDom());
 }
 
+// Function that takes in note id and display the note in editor
 function getNote(id) {
   let storage = localStorage.getItem('note');
   let content = JSON.parse(storage);
-
   content.forEach(e => {
-    console.log(e.id);
-    console.log(id);
     if (e.id === id) {
       quill.setContents(e.content);
-      console.log('yes');
-    } else {
-      console.log('continue');
+      console.log('getNote(): Open note: ' + e.id);
     }
-  });
+  }, false);
 }
 
 // Function that takes the current note's ID and saves it
@@ -75,7 +87,8 @@ function saveNote(id) {
   if (notesArray.find(n => n.id === id.toString())) {
     // If true: runt update function and save new note in memory
     updateNote(id);
-    localStorage.setItem('note', JSON.stringify(notesArray));
+    // Build and show the new updated DOM
+    showNotes(buildDom());
   } else {
     // If false: create a new note with contents of the editor
     const note = noteConstructor(quill.getContents());
@@ -88,14 +101,52 @@ function saveNote(id) {
     // Build and show the new updated DOM
     showNotes(buildDom());
 
-    console.log('no current, creating new note...');
+    console.log('saveNote(): No current, creating new note...');
+    console.log('saveNote(): Creates new note with ID: ' + note.id);
   }
 };
+
+function findID(id) {
+  if (id.length > 0) {
+    return notesArray.find(note => note.id === id);
+  } else {
+    console.error('findID(): No current ID');
+  }
+}
+
+function removeNote(id) {
+  const noteID = id;
+  notesArray[notesArray.indexOf(n => n.id === noteID)].pop();
+}
 
 // Function that takes ID and change the corresponding note's content, title and date
 function updateNote(id) {
   // UNDER CONSTRUCTION
-  notesArray.find(note => note.id === id).content = quill.getContents();
+  const note = findID(id);
+  note.title = noteTitle();
+  note.dateTime = getDate();
+  note.content = quill.getContents();
+  move(notesArray, notesArray.indexOf(note), 0);
+  localStorage.setItem('note', JSON.stringify(notesArray));
+  console.log('updateNote(): ' + note.title);
+}
+
+// Code from link that moves item from one spot to another https://www.w3resource.com/javascript-exercises/javascript-array-exercise-38.php
+function move(arr, old_index, new_index) {
+  while (old_index < 0) {
+    old_index += arr.length;
+  }
+  while (new_index < 0) {
+    new_index += arr.length;
+  }
+  if (new_index >= arr.length) {
+    var k = new_index - arr.length;
+    while ((k--) + 1) {
+      arr.push(undefined);
+    }
+  }
+  arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+  return arr;
 }
 
 // Get the note title from editor's first line
@@ -115,9 +166,23 @@ function guid() {
 }
 
 // Takes the buildDom function and appends all notes to the DOM
-function showNotes(build) {
+function showNotes() {
   // Appends all items to the DOM in #notes-output
-  build.forEach(el => notesOutput.appendChild(el));
+  buildDom().forEach(el => notesOutput.appendChild(el));
+}
+
+// Function that build the new domArray with notes
+function buildDom() {
+  // Clear the dom tree so no old ones are left
+  clearDom('#notes-output')
+
+  // Reset current domArray
+  domArray = [];
+
+  // Loop through them and push each item to new array
+  notesArray.forEach(el => domArray.push(createElement(el)));
+  // Return array
+  return domArray;
 }
 
 // Function that takes an object and creates a <li> with contents
@@ -131,7 +196,7 @@ function createElement(obj) {
 
   // Take information from obj and pass it in as innerHTML
   h4.innerHTML = obj.title;
-  date.innerHTML = "Date: ";
+  date.innerHTML = "Updated: ";
   span.innerHTML = obj.dateTime;
 
   // Append all elements to <li> in correct order
@@ -148,39 +213,34 @@ function createElement(obj) {
   return li;
 }
 
+
+
 // Function that removes the selected element's children
-function clearDom(elemID) {
+function clearDom() {
   // Select element.children and put it in a variable
-  const el = document.querySelector(elemID);
-  const children = el.children;
+  const children = notesOutput.children;
 
   // Check if element has children
   if (children.length > 0) {
     // If true: remove all children
-    for (let i = 0; i < children.length; i++) {
-      let parent = children[i].parentNode;
-      parent.removeChild(children[i]);
+    while (notesOutput.firstChild) {
+      notesOutput.removeChild(notesOutput.firstChild);
     }
   } else {
     // If false: nothing happens, (console.log)
-    console.log('No notes in DOM');
+    console.log('clearDom(): No notes in DOM');
   }
 }
 
-// Function that build the new domArray with notes
-function buildDom() {
-  // Clear the dom tree so no old ones are left
-  clearDom('#notes-output')
-  // Create the variable to store all notes
-  let domArray = [];
-  // Get all the notes from localStorage
+function start() {
   let storage = JSON.parse(localStorage.getItem('note'));
-  // Loop through them and push each item to new array
-  storage.forEach(el => domArray.push(createElement(el)));
-  // Return array
-  return domArray;
+  if (storage !== null) {
+    storage.forEach(el => notesArray.push(el));
+    showNotes(buildDom());
+  } else {
+    console.log('start(): No notes in memory.');
+  }
 }
-
 
 
 // Event Listeners
