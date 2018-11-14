0a9ebed3b636d6d2dcc1f0021f003892faf62d76
fabifors <fabian.forsstrom@gmail.com>
2018-11-14 14:58:37 +0100

    Changed so that new and save buttons animate in when menu button is pressed.

--- js/events.js
+++ js/events.js
@@ -3,6 +3,8 @@ const menuBTNClose = document.querySelector('#menu-btn-close');
 const sidebar = document.querySelector('#sidebar');
 const notes = document.querySelector('#notes');
 const lightBox = document.querySelector('.lightbox');
+const toolsBTN = document.querySelector('.tool-menu').children[0];
+const tools = document.querySelector('.tools');
 
 menuBTN.addEventListener('click', () => {
     showMenu(true);
@@ -28,3 +30,11 @@ function showMenu(bool) {
         console.error('Function has no argument or argument is not boolean.');
     }
 }
+
+function toolMenuOpen() {
+    toolsBTN.children[0].classList.toggle('open-menu1');
+    toolsBTN.children[1].classList.toggle('open-menu2');
+    toolsBTN.children[0].classList.toggle('close-menu1');
+    toolsBTN.children[1].classList.toggle('close-menu2');
+    tools.classList.toggle('tools-expanded');
+};
